024b7c595f4adfc7c87c6969417fa2f65034538c
Surendran Mahendran <surenspost@gmail.com>
2014-09-17 18:58:23 -0400

    Create all containers, files for game

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,6 +13,10 @@ class Game < ActiveRecord::Base
     [STATUS_WAITING, STATUS_QUEUED, STATUS_RUNNING, STATUS_COMPLETED]
   end
 
+  def snippet_name()
+    "#{game_type.name}-#{player_a.name}-#{player_b.name}-#{id}"
+  end
+
   def snippet_engine
     self.snippets.where(user: nil).first
   end
@@ -44,5 +48,46 @@ class Game < ActiveRecord::Base
     else
       `git clone #{game_type.engine_code_github} #{game_code_path}`
     end
+  end
+
+  def copy_game_files(snippet, subdirectory_path)
+    if snippet.container.nil?
+      Container.create! image: "codebin/#{game_type.allowed_player_images}", snippet: snippet
+    end
+
+    files = Dir["#{subdirectory_path}/**/*"]
+    files.each do |file|
+      next if File.directory? file
+      file_name = Pathname.new(file).relative_path_from(Pathname.new subdirectory_path)
+      contents = File.read file
+      snippet.container.create_host_file(file_name, contents)
+    end
+  end
+
+  def create_files_for_snippet(snippet)
+    # Copy all code needed to run player container
+    copy_game_files(snippet, "#{game_code_path}/player")
+
+    # Create user submitted code in container
+    snippet.container.create_host_file("player.#{snippet.extension}", snippet.code)
+  end
+
+  def create_files_for_engine()
+    if snippet_engine.nil?
+      Snippet.create! name: snippet_name, language: :python, game: self
+    end
+
+    copy_game_files(snippet_engine, "#{game_code_path}/engine")
+  end
+
+  def create_files
+    return if not ready?
+
+    checkout_game_code
+
+    create_files_for_snippet(snippet_a)
+    create_files_for_snippet(snippet_b)
+
+    create_files_for_engine()
   end
 end
